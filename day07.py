4b6473739df2f813543f8bc5936aa37ad3a3f491
Richard Schielek <richard.schielek@gmx.de>
2018-02-07 11:56:43 +0100

    a

--- day07.py
+++ day07.py
@@ -7,28 +7,28 @@ import re
 addresses = open('input/07.txt', 'r').read().splitlines()
 
 def supportsTLS(address):
-    checkOutsideBrackets = matchesABBA(outsideBrackets(address))
-    checkInsideBrackets = not matchesABBA(insideBrackets(address))
+    checkOutsideBrackets = matchesABBA(outsideBracketsOf(address))
+    checkInsideBrackets = not matchesABBA(insideBracketsOf(address))
     return checkOutsideBrackets and checkInsideBrackets
 
 def matchesABBA(string):
     return re.search(r'(\w)(?!\1)(\w)\2\1', string) != None
 
-def outsideBrackets(string):
+def outsideBracketsOf(string):
     return re.sub(r'(?<=\[)\w*(?=\])', '', string)
 
-def insideBrackets(string):
+def insideBracketsOf(string):
     return re.sub(r'(?:^|(?<=\]))\w*', '', string)
 
 def supportsSSL(address):
-    matches = matchesInABA(outsideBrackets(address))
+    matches = ABApatternsIn(outsideBracketsOf(address))
     for m in matches:
         reverse = m[1] + m[0] + m[1]
-        if re.search(reverse, insideBrackets(address)) != None:
+        if re.search(reverse, insideBracketsOf(address)) != None:
             return True
     return False
     
-def matchesInABA(string):
+def ABApatternsIn(string):
     matches = []
     while True:
         match = re.search(r'(\w)(?!\1)(\w)\1', string)
